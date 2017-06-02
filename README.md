d9a51efea0d853d596881246e47fdc27b64cc28f
Vagrant <Vagrant>
2017-06-02 04:23:53 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# cb_dvo_chefClient
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'cb_dvo_chefClient'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures cb_dvo_chefClient'
+long_description 'Installs/Configures cb_dvo_chefClient'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/cb_dvo_chefClient/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/cb_dvo_chefClient' if respond_to?(:source_url)
