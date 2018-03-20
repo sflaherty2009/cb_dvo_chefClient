1ccf71f4801123fddf4a5ab4444fa1e2063d22d5
dreweasland <drew_easland@trekbikes.com>
2018-03-20 14:03:50 -0500

    Updating supermarket version to try and fix issues found in Windows

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,9 @@ source_url 'https://bitbucket.org/trekbikes/cb_dvo_chefclient/src'
 issues_url 'https://bitbucket.org/trekbikes/cb_dvo_chefclient/issues'
 description 'Sets interval, splay and removes the Chef Validator'
 long_description 'See: https://bitbucket.org/trekbikes/cb_dvo_chefclient'
-version '1.0.6'
+version '1.0.7'
 
 chef_version '>= 12.1' if respond_to?(:chef_version)
-depends 'chef-client', '=8.1.2'
+depends 'chef-client', '= 10.0.2'
 supports 'centos'
+supports 'windows'
