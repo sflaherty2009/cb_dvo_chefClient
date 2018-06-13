245f8f8f8fb347e482e3402cb6b1bdd0f8692c96
dreweasland <drew_easland@trekbikes.com>
2018-06-13 16:05:47 -0500

    linting, again

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['chef_client']['log_dir'] = '/standard/sumologs/chef'
-default['ohai']['disabled_plugins'] = [':sessions',':passwd']
+default['ohai']['disabled_plugins'] = [':sessions', ':passwd']
 default['chef_client']['config']['client_key'] = '/etc/chef/client.pem'
 default['chef_client']['config']['validation_key'] = '/etc/chef/validation.pem'
 default['chef_client']['config']['ssl_verify_mode'] = ':verify_none'
