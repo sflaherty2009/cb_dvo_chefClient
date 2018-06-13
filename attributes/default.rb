4176435a4017627c338a4e4e5352081b5ea51952
dreweasland <drew_easland@trekbikes.com>
2018-06-13 16:05:12 -0500

    added disabled plugins and verify_api_cert and removed azure handlers

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,22 +1,8 @@
 default['chef_client']['log_dir'] = '/standard/sumologs/chef'
+default['ohai']['disabled_plugins'] = [':sessions',':passwd']
+default['chef_client']['config']['client_key'] = '/etc/chef/client.pem'
+default['chef_client']['config']['validation_key'] = '/etc/chef/validation.pem'
 default['chef_client']['config']['ssl_verify_mode'] = ':verify_none'
 default['chef_client']['config']['encrypted_data_bag_secret'] = '/etc/chef/encrypted_data_bag_secret'
 default['chef_client']['config']['trusted_certs_dir'] = '/etc/chef/trusted_certs'
-default['chef_client']['config']['start_handlers'] = [
-  {
-    class: 'AzureExtension::StartHandler',
-    arguments: ['"/var/lib/waagent/Chef.Bootstrap.WindowsAzure.LinuxChefClient-1210.12.110.1001"'],
-  },
-]
-default['chef_client']['config']['report_handlers'] = [
-  {
-    class: 'AzureExtension::StartHandler',
-    arguments: ['"/var/lib/waagent/Chef.Bootstrap.WindowsAzure.LinuxChefClient-1210.12.110.1001"'],
-  },
-]
-default['chef_client']['config']['exception_handlers'] = [
-  {
-    class: 'AzureExtension::StartHandler',
-    arguments: ['"/var/lib/waagent/Chef.Bootstrap.WindowsAzure.LinuxChefClient-1210.12.110.1001"'],
-  },
-]
+default['chef_client']['config']['verify_api_cert'] = false
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ source_url 'https://bitbucket.org/trekbikes/cb_dvo_chefclient/src'
 issues_url 'https://bitbucket.org/trekbikes/cb_dvo_chefclient/issues'
 description 'Sets Chef logrotation and deletes validation key'
 long_description 'See: https://bitbucket.org/trekbikes/cb_dvo_chefclient'
-version '1.2.1'
+version '1.2.2'
 
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
